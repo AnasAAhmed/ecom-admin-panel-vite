38bab81602b24a2c97ebb999f29d66894cfa3744
AnasAhmed <hafeezroll69@gmail.com>
2025-09-07 23:27:14 +0500

    try 2 image upload & update fixed & Sortign added

--- src/pages/products/new/page.tsx
+++ src/pages/products/new/page.tsx
@@ -1,17 +1,8 @@
-import ProductForm from '../../../components/products/ProductForm'
-import { useQuery } from '@tanstack/react-query';
-import { fetchCollections } from '../../../lib/api';
+import ProductForm from '../../../components/products/ProductForm';
 
 const NewProduct = () => {
-    const { data, isLoading, isError } = useQuery({
-        queryKey: ["collections"],
-        queryFn: fetchCollections,
-    });
-
-    if (typeof data ==='string' || isError) return <p className="p-4 text-red-500">Error Collections {JSON.stringify(data)}</p>;
-
     return (
-        <ProductForm collections={data} isLoading={isLoading} error={isError ? JSON.stringify(data) : ''} />
+        <ProductForm />
     )
 }
 
